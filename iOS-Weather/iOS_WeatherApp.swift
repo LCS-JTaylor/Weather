3117ee8ce5f6b352e9e5baa3a61afc784f59c14b
LCS-JTaylor <jtaylor@lcs.on.ca>
2021-10-02 10:24:14 -0400

    Added iOS target and also changed viewmodel variables

---
+++ iOS-Weather/iOS_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_WeatherApp.swift
+//  iOS-Weather
+//
+//  Created by Jackson Taylor on 2021-10-02.
+//
+
+import SwiftUI
+
+@main
+struct iOS_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
